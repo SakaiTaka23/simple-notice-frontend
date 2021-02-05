2eff07b3480215efcbbf62db4ecb1e3495e77ee1
SakaiTaka23 <tt3.sakai@gmail.com>
2021-02-05 15:08:21 +0900

    apiを叩けるようにした

--- pages/survey/[id].tsx
+++ pages/survey/[id].tsx
@@ -1,31 +1,35 @@
-import { GetServerSideProps } from 'next';
-import React, { FC } from 'react';
-import data from '../../data/mock_data';
+import React, { FC, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { formData } from '../../type/formType';
 import Questions from '../../components/Questions';
 
-type Prop = {
-  formData: formData;
-};
+const url = 'http://127.0.0.1/api/survey/';
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  //api作成して呼び出す
-  const formData = data;
-  return {
-    props: {
-      formData,
-    },
+const Form: FC = () => {
+  const [questions, setQuestions] = useState<formData>({} as formData);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const fetchQuestions = async () => {
+    const response = await fetch(url + id);
+    const newQuestions = await response.json();
+    setQuestions(newQuestions[0]);
   };
-};
 
-const Form: FC<Prop> = ({ formData }) => {
-  const { id, title, description, questions } = formData;
-  console.log(id, title, description, questions);
+  useEffect(() => {
+    if (id) {
+      fetchQuestions();
+    }
+  }, [id]);
+
+  const { title, description } = questions;
+
   return (
     <div>
+      <h1>survey</h1>
       <h1>{title}</h1>
       <h2>{description}</h2>
-      <Questions key={id} questions={questions} />
+      {/* <Questions key={id} questions={questions} /> */}
     </div>
   );
 };
