c2faa9c13881cebd53fc9353bc1367cf0f90025e
SakaiTaka23 <tt3.sakai@gmail.com>
2021-02-06 13:41:31 +0900

    axiosを採用してみた

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Surveys } from '../../type/api/surveyTypes';
-
-const url = 'http://127.0.0.1/api/survey';
 
 const Index = () => {
   const [surveys, setSurveys] = useState<Surveys>([]);
 
   const fetchSurveys = async () => {
-    const response = await fetch(url);
-    const newSurveys = await response.json();
+    const url = 'http://127.0.0.1/api/survey';
+    const response = await axios.get(url);
+    const newSurveys = response.data;
     setSurveys(newSurveys);
   };
 
