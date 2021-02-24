6c25d2fd2cf65b8bff8e369615ec23c5178d9b0a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-02-24 21:22:07 +0900

    landingページの感覚を開けた

--- components/Landing/AppealCreate.tsx
+++ components/Landing/AppealCreate.tsx
@@ -1,21 +1,23 @@
-import { Grid, Paper, Typography } from '@material-ui/core';
+import { Box, Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import { useStyles } from '../../theme/Theme';
 
 const AppealCreate = () => {
   const classes = useStyles();
   return (
-    <Paper className={classes.glass}>
-      <Grid container direction='row' justify='center' alignItems='center' spacing={10}>
-        <Grid item xs={5}>
-          <img src='/svg/undraw_customer_survey.svg' alt='hero img' width='300' height='300' />
+    <Box paddingY={1}>
+      <Paper className={classes.glass}>
+        <Grid container direction='row' justify='center' alignItems='center' spacing={10}>
+          <Grid item xs={5}>
+            <img src='/svg/undraw_customer_survey.svg' alt='hero img' width='300' height='300' />
+          </Grid>
+          <Grid item xs={5}>
+            <Typography variant='h4'>Create Serveys</Typography>
+            <Typography variant='h5'>Create Serveys In Your Own Need</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={5}>
-          <Typography variant='h4'>Create Serveys</Typography>
-          <Typography variant='h5'>Create Serveys In Your Own Need</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Box>
   );
 };
 
--- components/Landing/HeroSection.tsx
+++ components/Landing/HeroSection.tsx
@@ -1,21 +1,23 @@
-import { Grid, Paper, Typography } from '@material-ui/core';
+import { Box, Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import { useStyles } from '../../theme/Theme';
 
 const HeroSection = () => {
   const classes = useStyles();
   return (
-    <Paper className={classes.glass}>
-      <Grid container direction='row' justify='center' alignItems='center' spacing={10}>
-        <Grid item xs={5}>
-          <Typography variant='h4'>Servey Simply</Typography>
-          <Typography variant='h5'>Take Serveys Of Things You Want To Know</Typography>
+    <Box paddingY={1} paddingTop={2}>
+      <Paper className={classes.glass}>
+        <Grid container direction='row' justify='center' alignItems='center' spacing={10}>
+          <Grid item xs={5}>
+            <Typography variant='h4'>Servey Simply</Typography>
+            <Typography variant='h5'>Take Serveys Of Things You Want To Know</Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <img src='/svg/undraw_survey.svg' alt='hero img' width='300' height='300' />
+          </Grid>
         </Grid>
-        <Grid item xs={5}>
-          <img src='/svg/undraw_survey.svg' alt='hero img' width='700' height='700' />
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Box>
   );
 };
 
