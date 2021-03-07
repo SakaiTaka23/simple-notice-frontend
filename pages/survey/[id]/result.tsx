3f10f8c227f7391e541bd85911651339a1dab11c
SakaiTaka23 <tt3.sakai@gmail.com>
2021-03-07 17:51:23 +0900

    結果表示ページもSSRに対応させた

--- pages/survey/[id]/result.tsx
+++ pages/survey/[id]/result.tsx
@@ -1,58 +1,55 @@
 import { Box, Grid, Paper, Typography } from '@material-ui/core';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import React, { FC } from 'react';
 import Results from '../../../components/Results';
 import { useStyles } from '../../../theme/Theme';
-import { SurveyResult } from '../../../type/api/surveyResultTypes';
+import { QuestionResult } from '../../../type/api/surveyResultTypes';
 
-const Result = () => {
+type Props = {
+  title: string;
+  description: string;
+  owner: string;
+  questions: QuestionResult[];
+};
+
+const Result: FC<Props> = ({ title, description, owner, questions }) => {
   const classes = useStyles();
-  const [resultData, setResultData] = useState<SurveyResult>({
-    title: 'title',
-    description: 'desc',
-    owner: 'owner',
-    questions: [],
-  });
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const id = router.query.id;
-
-  const fetchResultData = async () => {
-    const url = `http://127.0.0.1/api/survey/${id}/result`;
-    const response = await axios.get(url);
-    const newResultData = response.data;
-    setResultData(newResultData);
-  };
-
-  useEffect(() => {
-    if (id) {
-      fetchResultData();
-      setLoading(false);
-    }
-  }, [id]);
-
-  const { questions } = resultData;
-  console.log(questions);
-
-  if (loading || id === undefined) {
-    return <h1>loading</h1>;
-  }
 
   return (
     <div style={{ padding: 70 }}>
       <Grid container direction='column' justify='center' spacing={6}>
         <Box py={3}>
           <Paper className={classes.glass}>
-            <Typography variant='h3'>{resultData.title}</Typography>
-            <Typography variant='h4'>{resultData.description}</Typography>
-            <Typography variant='h5'>{resultData.owner}</Typography>
+            <Typography variant='h3'>{title}</Typography>
+            <Typography variant='h4'>{description}</Typography>
+            <Typography variant='h5'>{owner}</Typography>
           </Paper>
         </Box>
         <Results key='1' questions={questions} />
       </Grid>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.params?.id as string;
+  const response = await fetch(`http://127.0.0.1/api/survey/${id}/result`);
+
+  if (response.status === 400) {
+    return { notFound: true };
+  }
+
+  const data = await response.json();
+  const { title, description, owner, questions } = data;
+
+  return {
+    props: {
+      title,
+      description,
+      owner,
+      questions,
+    },
+  };
 };
 
 export default Result;
